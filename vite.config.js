40e7c3977173843d5f890bf8c540eb5c3f779971
ekadanis <ekadanis879@gmail.com>
2025-07-17 11:40:55 +0700

    add base /

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: "/",
   server: {
     port: 3000, // Ubah port dari default 5173 ke 3000
   },
-})
+});
